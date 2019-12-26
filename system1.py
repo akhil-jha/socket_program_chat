a17dc825934e17911c9f4a070b2adf23099a75be
akhil-jha <38880644+akhil-jha@users.noreply.github.com>
2019-12-26 17:53:19 +0530

    Add files via upload

---
+++ system1.py
@@ -0,0 +1,20 @@
+import socket
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 11999
+mysocket.bind(("10.65.33.16", port))
+mysocket.listen(1)
+clientsocket, addr = mysocket.accept()
+print("Got a connection from %s" % str(addr))
+flag = 1
+while flag == 1:
+    message_inbound = clientsocket.recv(1024)
+    print("Peer message: ", message_inbound.decode("utf-8"), end='\n')
+    message_outbound = input("Enter your message(For exit: exit() ):  ")
+    temp = message_outbound.strip(" ")
+    temp = temp.split(" ")
+    if "exit()" in temp:
+        flag = 0
+    else:
+        clientsocket.send(bytes(message_outbound, "utf-8"))
+
+mysocket.close()
---
+++ system2.py
@@ -0,0 +1,19 @@
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 11999
+mysocket.connect(("10.65.33.16", port))
+flag = 1
+while flag == 1:
+    message_outbound = input("Enter your message (For exit: exit() ):  ")
+    temp = message_outbound.strip(" ")
+    temp = temp.split(" ")
+    if "exit()" in temp:
+        flag = 0
+        mysocket.close()
+        exit(1)
+    else:
+        mysocket.send(bytes(message_outbound, "utf-8"))
+    message_inbound = mysocket.recv(1024)
+    print("Peer message: ", message_inbound.decode("utf-8"), end='\n')
+mysocket.close()
